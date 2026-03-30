ca80ac4a1548e5ec058d0002ed99d2b9cf9a4d7c
GitHub Action <action@github.com>
2026-03-30 18:31:58 +0000

    Regenerate artifacts from biolink-model.yaml

--- src/biolink_model/datamodel/model.py
+++ src/biolink_model/datamodel/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-03-26T23:26:04
+# Generation date: 2026-03-30T18:30:16
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/vocab/
